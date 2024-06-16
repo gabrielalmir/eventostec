8ac6d2a1a6d6dc66f8b5b88cc99d099bc8793fbb
gabrielalmir <gabr.almir@gmail.com>
2024-06-15 22:26:44 -0300

    fix: typos on event column names

--- src/main/java/com/eventostec/api/domain/event/Event.java
+++ src/main/java/com/eventostec/api/domain/event/Event.java
@@ -17,8 +17,8 @@ public class Event {
     @GeneratedValue
     private UUID id;
 
-    private String name;
-    private String email;
+    private String title;
+    private String description;
     private String imageUrl;
     private String eventUrl;
     private Boolean remote;
