6e02df21a3d70983176f4b690f70e1caf3ce20ea
gabrielalmir <gabr.almir@gmail.com>
2024-06-23 07:59:06 -0300

    feat: remove commentary event model

--- src/event/models.rs
+++ src/event/models.rs
@@ -1,17 +1,5 @@
 use serde::Serialize;
 
-
-// CREATE TABLE event (
-//     id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-//     title VARCHAR(100) NOT NULL,
-//     description VARCHAR(250) NOT NULL,
-//     image_url VARCHAR(100) NOT NULL,
-//     event_url VARCHAR(100) NOT NULL,
-//     date TIMESTAMP NOT NULL,
-//     remote BOOLEAN NOT NULL
-// );
-
-
 #[derive(Serialize)]
 pub struct Event {
     pub id: uuid::Uuid,
